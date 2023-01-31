9b551e97461e6627a05ddcd6914fc872155277bc
StarFarer <32253980+StarFarer-cli@users.noreply.github.com>
2023-01-30 19:25:37 -0800

    rust cargowq

---
+++ rust/0x01/target/debug/deps/projecteuler_0x00-e725f1cfa39c251e.d
@@ -0,0 +1,5 @@
+/home/luna/Projects/StarFarer/verbose-projecteuler/rust/0x01/target/debug/deps/projecteuler_0x00-e725f1cfa39c251e: src/main.rs
+
+/home/luna/Projects/StarFarer/verbose-projecteuler/rust/0x01/target/debug/deps/projecteuler_0x00-e725f1cfa39c251e.d: src/main.rs
+
+src/main.rs:
---
+++ rust/0x01/target/debug/projecteuler_0x00.d
@@ -0,0 +1,1 @@
+/home/luna/Projects/StarFarer/verbose-projecteuler/rust/0x01/target/debug/projecteuler_0x00: /home/luna/Projects/StarFarer/verbose-projecteuler/rust/0x01/src/main.rs
